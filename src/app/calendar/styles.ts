3bec55276984b57e9aa30a78a079d0d29ef53330
Alex Ye <albertexye@gmail.com>
2024-10-06 08:56:01 -0400

    feat: Use Agenda on a separate screeb

---
+++ src/app/calendar/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: 'white',
+        flex: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginRight: 10,
+        marginTop: 17
+    },
+    emptyDate: {
+        height: 15,
+        flex: 1,
+        paddingTop: 30
+    },
+    customDay: {
+        margin: 10,
+        fontSize: 24,
+        color: 'green'
+    },
+    dayItem: {
+        marginLeft: 34
+    }
+});
+
+export default styles;
