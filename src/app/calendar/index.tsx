a15ac88d7767489f0ce736e6957dfbc8a2e383c3
Alex Ye <albertexye@gmail.com>
2024-10-09 15:51:10 -0400

    Load calendar events from the global constants

--- src/app/calendar/index.tsx
+++ src/app/calendar/index.tsx
@@ -3,6 +3,7 @@ import { View, Text, TouchableOpacity, Alert } from 'react-native';
 
 import * as NativeCalendar from 'react-native-calendars';
 
+import { EVENTS } from '@/globals/constants.ts';
 import styles from './styles';
 
 function timeToString(time: number) {
@@ -10,40 +11,31 @@ function timeToString(time: number) {
     return date.toISOString().split('T')[0];
 }
 
+function loadEvents() {
+	const items = {};
+	for (const event of EVENTS) {
+	    const dateStr = timeToString(event.date.valueOf());
+	    if (dateStr in items) {
+		    items[dateStr].push(event);
+	    }
+            else {
+		    items[dateStr] = [event];
+	    }
+	}
+	return items;
+}
+
 export default function () {
-    const [items, setItems] = useState<NativeCalendar.AgendaSchedule>({});
+    const [items, setItems] = useState<NativeCalendar.AgendaSchedule>(loadEvents());
 
     const loadItems = (day: NativeCalendar.DateData) => {
-        const strTime = timeToString(day.timestamp);
-        items[strTime] = [];
-        setItems(items);
-        // const itemsCopy = items;
-
-        // setTimeout(() => {
-        //     for (let i = -15; i < 85; i++) {
-        //         const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        //         const strTime = timeToString(time);
-
-        //         if (!itemsCopy[strTime]) {
-        //             itemsCopy[strTime] = [];
-
-        //             const numItems = Math.floor(Math.random() * 3 + 1);
-        //             for (let j = 0; j < numItems; j++) {
-        //                 itemsCopy[strTime].push({
-        //                     name: 'Item for ' + strTime + ' #' + j,
-        //                     height: Math.max(50, Math.floor(Math.random() * 150)),
-        //                     day: strTime
-        //                 });
-        //             }
-        //         }
-        //     }
-
-        //     const newItems: NativeCalendar.AgendaSchedule = {};
-        //     Object.keys(itemsCopy).forEach(key => {
-        //         newItems[key] = itemsCopy[key];
-        //     });
-        //     setItems(newItems);
-        // }, 1000);
+	    for (let i = -14; i <= 14; ++i) {
+		    const date = timeToString(day.timestamp + i * 24 * 60 * 60 * 1000);
+		    if (!(date in items)) {
+			    items[date] = [];
+		    }
+	    }
+	    setItems(items);
     };
 
     const renderItem = (reservation: NativeCalendar.AgendaEntry, isFirst: boolean) => {
@@ -52,17 +44,20 @@ export default function () {
 
         return (
             <TouchableOpacity
-                style={[styles.item, { height: reservation.height }]}
-                onPress={() => Alert.alert(reservation.name)}
+                style={ styles.item }
+                onPress={ () => Alert.alert(reservation.name) }
             >
-                <Text style={{ fontSize, color }}>{reservation.name}</Text>
+                <Text style={ { fontSize, color } }>{ reservation.name }</Text>
+                <Text style={ { fontSize, color } }>{ reservation.location }</Text>
+                <Text style={ { fontSize, color } }>{ reservation.description }</Text>
+                <Text style={ { fontSize, color } }>{ reservation.type }</Text>
             </TouchableOpacity>
         );
     };
 
     const renderEmptyDate = () => {
         return (
-            <View style={styles.emptyDate}>
+            <View style={ styles.emptyDate }>
                 <Text>This is empty date!</Text>
             </View>
         );
@@ -74,25 +69,13 @@ export default function () {
 
     return (
         <NativeCalendar.Agenda
-            items={items}
-            loadItemsForMonth={loadItems}
-            selected={'2024-05-16'}
-            renderItem={renderItem}
-            renderEmptyDate={renderEmptyDate}
-            rowHasChanged={rowHasChanged}
-            showClosingKnob={true}
-            markingType={'period'}
-            markedDates={{
-                // '2017-05-08': { textColor: '#43515c' },
-                // '2017-05-09': { textColor: '#43515c' },
-                // '2017-05-14': { startingDay: true, endingDay: true, color: 'blue' },
-                // '2017-05-21': { startingDay: true, color: 'blue' },
-                // '2017-05-22': { endingDay: true, color: 'gray' },
-                // '2017-05-24': { startingDay: true, color: 'gray' },
-                // '2017-05-25': { color: 'gray' },
-                // '2017-05-26': { endingDay: true, color: 'gray' }
-            }}
-        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+            items={ items }
+            loadItemsForMonth={ loadItems }
+            selected={ timeToString(Date.now()) }
+            renderItem={ renderItem }
+            renderEmptyDate={ renderEmptyDate }
+            rowHasChanged={ rowHasChanged }
+            showClosingKnob={ true }
         />
-    )
+    );
 }
