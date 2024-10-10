0ad7bcdae93cb5f1af34f789d01a24507be1eb70
Alex Ye <albertexye@gmail.com>
2024-10-10 17:04:09 -0400

    feat: Add dark mode support to calendar

--- src/app/calendar/index.tsx
+++ src/app/calendar/index.tsx
@@ -3,8 +3,9 @@ import { View, Text, TouchableOpacity, Alert } from 'react-native';
 
 import * as NativeCalendar from 'react-native-calendars';
 
-import { EVENTS } from '@/globals/constants.ts';
+import { EVENTS } from '@/globals/constants';
 import styles from './styles';
+import globalStyles from '@/globals/globalStyles';
 
 function timeToString(time: number) {
     const date = new Date(time);
@@ -12,45 +13,42 @@ function timeToString(time: number) {
 }
 
 function loadEvents() {
-	const items = {};
-	for (const event of EVENTS) {
-	    const dateStr = timeToString(event.date.valueOf());
-	    if (dateStr in items) {
-		    items[dateStr].push(event);
-	    }
-            else {
-		    items[dateStr] = [event];
-	    }
-	}
-	return items;
+    const items: { [key: string]: Array<CalvinEvent> } = {};
+    for (const event of EVENTS) {
+        const dateStr = timeToString(event.date.valueOf());
+        if (dateStr in items) {
+            items[dateStr].push(event);
+        }
+ else {
+            items[dateStr] = [event];
+        }
+    }
+    return items;
 }
 
 export default function () {
-    const [items, setItems] = useState<NativeCalendar.AgendaSchedule>(loadEvents());
+    const [items, setItems] = useState<{ [key: string]: Array<CalvinEvent> }>(loadEvents());
 
     const loadItems = (day: NativeCalendar.DateData) => {
-	    for (let i = -14; i <= 14; ++i) {
-		    const date = timeToString(day.timestamp + i * 24 * 60 * 60 * 1000);
-		    if (!(date in items)) {
-			    items[date] = [];
-		    }
-	    }
-	    setItems(items);
+        for (let i = -14; i <= 14; ++i) {
+            const date = timeToString(day.timestamp + i * 24 * 60 * 60 * 1000);
+            if (!(date in items)) {
+                items[date] = [];
+            }
+        }
+        setItems(items);
     };
 
-    const renderItem = (reservation: NativeCalendar.AgendaEntry, isFirst: boolean) => {
-        const fontSize = isFirst ? 16 : 14;
-        const color = isFirst ? 'black' : '#43515c';
-
+    const renderItem = (reservation: CalvinEvent) => {
         return (
             <TouchableOpacity
                 style={ styles.item }
                 onPress={ () => Alert.alert(reservation.name) }
             >
-                <Text style={ { fontSize, color } }>{ reservation.name }</Text>
-                <Text style={ { fontSize, color } }>{ reservation.location }</Text>
-                <Text style={ { fontSize, color } }>{ reservation.description }</Text>
-                <Text style={ { fontSize, color } }>{ reservation.type }</Text>
+                <Text style={ styles.text }>{ reservation.name }</Text>
+                <Text style={ styles.text }>{ reservation.location }</Text>
+                <Text style={ styles.text }>{ reservation.description }</Text>
+                <Text style={ styles.text }>{ reservation.type }</Text>
             </TouchableOpacity>
         );
     };
@@ -58,7 +56,7 @@ export default function () {
     const renderEmptyDate = () => {
         return (
             <View style={ styles.emptyDate }>
-                <Text>This is empty date!</Text>
+                <Text style={ styles.text }>This is empty date!</Text>
             </View>
         );
     };
@@ -76,6 +74,14 @@ export default function () {
             renderEmptyDate={ renderEmptyDate }
             rowHasChanged={ rowHasChanged }
             showClosingKnob={ true }
+            theme={ {
+                reservationsBackgroundColor: globalStyles.black,
+                calendarBackground: globalStyles.black,
+                agendaDayTextColor: 'yellow',
+                agendaDayNumColor: 'green',
+                agendaTodayColor: 'red',
+                agendaKnobColor: 'blue'
+            } }
         />
     );
 }
