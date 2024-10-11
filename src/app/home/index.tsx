89bd55145d2d150fcb81ff7f37e47ce84843bf75
Ray Cai Flanagan <hjkareus@gmail.com>
2024-10-11 15:21:22 -0400

    Implement filter functionality

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,18 +1,23 @@
+import { useState } from 'react';
 import { ScrollView, View, Text, Image } from 'react-native';
 
 import EvilIcons from '@expo/vector-icons/EvilIcons';
 
 import Input from '@/components/input';
 import Button from '@/components/button';
-import EventRecommendation from '@/components/eventRecommendation';
 import Logo from '@/components/texts/logo';
 
+import EventRecommendation from '@/components/eventRecommendation';
+import Event from '@/components/event';
+
 import globalStyles from '@/globals/globalStyles';
-import { CATEGORIES } from '@/globals/constants';
+import { CATEGORIES, EVENTS } from '@/globals/constants';
 
 import styles from './styles';
 
 export default function Home() {
+    const [fitler, updateFilter] = useState('');
+
     return (
         <ScrollView
             contentContainerStyle={ styles.container }
@@ -55,6 +60,9 @@ export default function Home() {
                                 backgroundColor: globalStyles.veryDarkGray
                             }
                         }
+                        onChangeText={
+                            e => updateFilter(e)
+                        }
                     />
 
                     <ScrollView
@@ -72,7 +80,9 @@ export default function Home() {
                                 (c, i) => (
                                     <Button
                                         key={ i }
-                                        onPress={ () => {} }
+                                        onPress={
+                                            () => updateFilter(c.split(' ')[1].replaceAll(' ', '')) // get rid of emojis
+                                        }
                                     >
                                         <Text
                                             style={
@@ -91,8 +101,48 @@ export default function Home() {
                 </View>
 
                 <View style={ styles.recommendationsContentContainer }>
-                    <EventRecommendation title="Upcoming Events" horizontalScroll={ true } />
-                    <EventRecommendation title="Recommended for You" eventCardType="price" horizontalScroll={ false } />
+                    {
+                        fitler.replaceAll(' ', '').length > 0 ? (
+                            <>
+                                {
+                                    (function filterEvents() {
+                                        const filteredEvents = EVENTS.filter(
+                                            e => (
+                                                e.name.toLowerCase().includes(fitler.toLowerCase()) ||
+                                                e.location.toLowerCase().includes(fitler.toLowerCase()) ||
+                                                e.description.toLowerCase().includes(fitler.toLowerCase()) ||
+                                                e.type.toLowerCase().includes(fitler.toLowerCase())
+                                            )
+                                        ).map(
+                                            (e, i) => <Event key={ i } { ...e } />
+                                        );
+
+                                        if(filteredEvents.length === 0)
+                                            return (
+                                                <Text
+                                                    style={
+                                                        {
+                                                            color: globalStyles.white,
+                                                            fontSize: 16,
+                                                            textAlign: 'center'
+                                                        }
+                                                    }
+                                                >
+                                                    No events found...
+                                                </Text>
+                                            );
+
+                                        return filteredEvents;
+                                    })()
+                                }
+                            </>
+                        ) : (
+                            <>
+                                <EventRecommendation title="Upcoming Events" horizontalScroll={ true } />
+                                <EventRecommendation title="Recommended for You" eventCardType="price" horizontalScroll={ false } />
+                            </>
+                        )
+                    }
                 </View>
             </View>
         </ScrollView>
