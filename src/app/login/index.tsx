367da4ed7e2605dfb9e5f55a8eeae518f4d1449c
Ray Cai Flanagan <hjkareus@gmail.com>
2024-10-11 15:22:29 -0400

    lints

--- src/app/login/index.tsx
+++ src/app/login/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text } from 'react-native';
 
 import Divider from '@/components/divider';
 import Button from '@/components/button';
