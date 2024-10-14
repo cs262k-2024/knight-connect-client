8be90c65a5397963d8fde893589512a3041e783b
Ray Cai Flanagan <hjkareus@gmail.com>
2024-10-14 13:12:43 -0400

    update incorrect stylings on mobile

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -52,7 +52,8 @@ export default function Home() {
                         ) }
                         containerStyle={
                             {
-                                backgroundColor: globalStyles.veryDarkGray
+                                backgroundColor: globalStyles.veryDarkGray,
+                                width: '100%'
                             }
                         }
                     />
