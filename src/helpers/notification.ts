bcab773b0ad7aaceb02160e2c3c63f3a03a68ca1
Alex Ye <albertexye@gmail.com>
2024-10-15 16:36:18 -0400

    feat: add essential notification function
    call "scheduleNotification(title, body)" to push a simple notification

---
+++ src/helpers/notification.ts
@@ -0,0 +1,80 @@
+import { Platform } from 'react-native';
+
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+import Constants from 'expo-constants';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+    }),
+});
+
+async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === 'android') {
+        await Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
+    }
+
+    if (Device.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        try {
+            const projectId =
+                Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+            if (!projectId) {
+                throw new Error('Project ID not found');
+            }
+            token = (
+                await Notifications.getExpoPushTokenAsync({
+                    projectId,
+                })
+            ).data;
+            console.log(token);
+        }
+        catch (e) {
+            token = `${e}`;
+        }
+    }
+    else {
+        alert('Must use physical device for Push Notifications');
+    }
+
+    return token;
+}
+
+// register for push notifications on app start
+(async () => {
+    try {
+        await registerForPushNotificationsAsync();
+    } catch (e) {
+        console.log(`failed to register for push notifications: ${e}`);
+    }
+})();
+
+// call this function to schedule a notification
+export const scheduleNotification = async (title: string, body: string) => {
+    await Notifications.scheduleNotificationAsync({
+        content: {
+            title,
+            body,
+        },
+        trigger: null,
+    });
+};
