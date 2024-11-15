7472228c97ab3cbe198159842cbdfb18b8abf114
Nana Osei AY <ay@AYs-MacBook-Pro.local>
2024-11-14 20:53:17 -0500

    change back button routing

--- src/app/eventPage/_layout.tsx
+++ src/app/eventPage/_layout.tsx
@@ -24,7 +24,7 @@ export default function EventPageLayout() {
                                         Haptics.ImpactFeedbackStyle.Soft,
                                     );
 
-                                    router.navigate('/(tabs)/home');
+                                    router.back();
                                 } }
                             >
                                 <Icon
