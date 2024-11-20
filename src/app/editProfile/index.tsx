549cd6f4438526158416292867fd6c5213fe51d2
Nana Osei AY <ay@AYs-MacBook-Pro.local>
2024-11-20 18:36:29 -0500

    lint

--- src/app/editProfile/index.tsx
+++ src/app/editProfile/index.tsx
@@ -66,108 +66,108 @@ export default function EditProfilel() {
     return (
         <KeyboardAvoidingView
             behavior="padding"
-            enabled={true}
-            style={styles.container}
+            enabled={ true }
+            style={ styles.container }
         >
-            <SafeAreaView style={styles.container}>
-                <View style={styles.topButtonsContainer}>
+            <SafeAreaView style={ styles.container }>
+                <View style={ styles.topButtonsContainer }>
                     <View>
                         <TouchableOpacity
-                            onPress={() => {
+                            onPress={ () => {
                                 router.back();
-                            }}
+                            } }
                         >
                             <Icon
                                 name="x"
                                 type="feather"
-                                color={globalStyles.white}
+                                color={ globalStyles.white }
                             />
                         </TouchableOpacity>
                     </View>
                     <View>
                         <TouchableOpacity
-                            onPress={() => {
+                            onPress={ () => {
                                 Haptics.impactAsync(
                                     Haptics.ImpactFeedbackStyle.Soft,
                                 );
                                 save();
-                            }}
+                            } }
                         >
-                            <Text style={styles.saveText}>Save</Text>
+                            <Text style={ styles.saveText }>Save</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
                 <ScrollView
-                    contentContainerStyle={{ flexGrow: 1 }}
+                    contentContainerStyle={ { flexGrow: 1 } }
                     keyboardShouldPersistTaps="handled"
                 >
-                    <View style={styles.avatarContainer}>
+                    <View style={ styles.avatarContainer }>
                         <Avatar
-                            size={88}
+                            size={ 88 }
                             rounded
-                            source={{
+                            source={ {
                                 uri: 'https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-800x525.jpg',
-                            }}
+                            } }
                         />
                     </View>
                     <View>
-                        <View style={styles.center}>
-                            <Text style={styles.caption}>Username</Text>
+                        <View style={ styles.center }>
+                            <Text style={ styles.caption }>Username</Text>
                             <Input
-                                cursorColor={globalStyles.gray}
+                                cursorColor={ globalStyles.gray }
                                 placeholder="Username"
                                 textAlign="center"
-                                placeholderTextColor={globalStyles.darkGray}
-                                value={username}
-                                onChangeText={updateUsername}
-                                inputContainerStyle={styles.inputContainer}
-                                inputStyle={styles.input}
+                                placeholderTextColor={ globalStyles.darkGray }
+                                value={ username }
+                                onChangeText={ updateUsername }
+                                inputContainerStyle={ styles.inputContainer }
+                                inputStyle={ styles.input }
                             />
                         </View>
-                        <View style={styles.center}>
-                            <Text style={styles.caption}>About</Text>
+                        <View style={ styles.center }>
+                            <Text style={ styles.caption }>About</Text>
                             <Input
-                                multiline={true}
+                                multiline={ true }
                                 placeholder="Enter a bio"
                                 textAlign="center"
-                                placeholderTextColor={globalStyles.darkGray}
-                                value={bio}
-                                onChangeText={updateBio}
-                                inputContainerStyle={styles.inputContainer}
-                                inputStyle={styles.input}
+                                placeholderTextColor={ globalStyles.darkGray }
+                                value={ bio }
+                                onChangeText={ updateBio }
+                                inputContainerStyle={ styles.inputContainer }
+                                inputStyle={ styles.input }
                             />
                         </View>
                     </View>
 
-                    <View style={styles.section}>
-                        <Text style={styles.sectionTitle}>About Me</Text>
+                    <View style={ styles.section }>
+                        <Text style={ styles.sectionTitle }>About Me</Text>
 
                         <Input
-                            containerStyle={containerInputStyle}
-                            inputStyle={inputStyle}
-                            placeholder={bio}
-                            value={bio}
-                            onChangeText={updateBio}
+                            containerStyle={ containerInputStyle }
+                            inputStyle={ inputStyle }
+                            placeholder={ bio }
+                            value={ bio }
+                            onChangeText={ updateBio }
                         />
                     </View>
 
-                    <InterestsBottomSheetModal ref={bottomSheetRef} />
+                    <InterestsBottomSheetModal ref={ bottomSheetRef } />
 
-                    <View style={styles.section}>
-                        <View style={styles.selectInterestsHeader}>
-                            <Text style={styles.sectionTitle}>
-                                Your Interests ({user.preferences.length})
+                    <View style={ styles.section }>
+                        <View style={ styles.selectInterestsHeader }>
+                            <Text style={ styles.sectionTitle }>
+                                Your Interests ({ user.preferences.length })
                             </Text>
 
                             <TouchableOpacity
-                                style={[
+                                style={ [
                                     styles.interestContainer,
                                     {
                                         backgroundColor: globalStyles.lightBlue,
                                         borderColor: globalStyles.lightBlue,
                                     },
-                                ]}
-                                onPress={handlePresentBottomSheet}
+                                ] }
+                                onPress={ handlePresentBottomSheet }
                                 // onPress={() => {
                                 //     Haptics.impactAsync(
                                 //         Haptics.ImpactFeedbackStyle.Soft,
@@ -178,29 +178,29 @@ export default function EditProfilel() {
                                 // }}
                             >
                                 <Text
-                                    style={[
+                                    style={ [
                                         styles.interestText,
                                         { color: globalStyles.white },
-                                    ]}
+                                    ] }
                                 >
                                     Add +
                                 </Text>
                             </TouchableOpacity>
                         </View>
 
-                        <View style={styles.row}>
-                            {user.preferences.map((interest) => {
+                        <View style={ styles.row }>
+                            { user.preferences.map((interest) => {
                                 return (
                                     <View
-                                        key={interest}
-                                        style={styles.interestContainer}
+                                        key={ interest }
+                                        style={ styles.interestContainer }
                                     >
-                                        <Text style={styles.interestText}>
-                                            {interest}
+                                        <Text style={ styles.interestText }>
+                                            { interest }
                                         </Text>
                                     </View>
                                 );
-                            })}
+                            }) }
                         </View>
                     </View>
                 </ScrollView>
