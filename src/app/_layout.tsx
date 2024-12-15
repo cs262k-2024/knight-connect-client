f32657da43d9bf219b9a9ecf59d9e75a69b78b1f
Alex Ye <albertexye@gmail.com>
2024-12-15 17:37:57 -0500

    docs: app layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,16 @@
+/**
+ * RootLayout component that sets up the main application layout.
+ * It includes font loading, splash screen handling, and context providers.
+ * 
+ * @file /home/alex/Documents/Projects/calvin/k/knight-connect-client/src/app/_layout.tsx
+ */
+
 import { useEffect } from 'react';
-
 import { useFonts } from 'expo-font';
-
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-
 import UserContextProvider from '@/contexts/userContext';
-
 import globalStyles from '@/globals/globalStyles';
-
 import 'react-native-reanimated';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
@@ -16,6 +18,12 @@ import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+/**
+ * RootLayout component that sets up the main application layout.
+ * It includes font loading, splash screen handling, and context providers.
+ * 
+ * @returns {JSX.Element | null} The main application layout or null if fonts are not loaded.
+ */
 export default function RootLayout() {
     const [loaded] = useFonts({
         Fredoka: require('../assets/fonts/Fredoka-VariableFont.ttf'),
